25d07bcc1e539461e26a7ce8313e1cd6167bc560
jxcmj92 <1757111322@qq.com>
2019-04-04 19:00:26 +0800

    完善excle工具类

--- src/main/java/com/springboot/utils/excel/ExcelUtil.java
+++ src/main/java/com/springboot/utils/excel/ExcelUtil.java
@@ -2,11 +2,13 @@ package com.springboot.utils.excel;
 
 import com.alibaba.excel.EasyExcelFactory;
 import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
 import com.alibaba.excel.metadata.BaseRowModel;
 import com.alibaba.excel.metadata.Sheet;
-import com.springboot.utils.excel.test.TableHeaderExcelProperty;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
@@ -33,19 +35,24 @@ public class ExcelUtil {
       initSheet.setAutoWidth(Boolean.TRUE);
    }
 
+   /**
+    * 读取少于1000行数据
+    * @param filePath 文件绝对路径
+    * @return
+    */
    public static List<Object> readLessThan1000Row(String filePath){
-      return readLessThan1000Row(filePath,null);
+      return readLessThan1000RowBySheet(filePath,null);
    }
 
    /**
-    * 读07版小于1000行数据
+    * 读小于1000行数据, 带样式
     * filePath 文件绝对路径
     * initSheet ：
     *      sheetNo: sheet页码，默认为1
     *      headLineMun: 从第几行开始读取数据，默认为0, 表示从第一行开始读取
     *      clazz: 返回数据List<Object> 中Object的类名
     */
-   public static List<Object> readLessThan1000Row(String filePath, Sheet sheet){
+   public static List<Object> readLessThan1000RowBySheet(String filePath, Sheet sheet){
       if(!StringUtils.hasText(filePath)){
          return null;
       }
@@ -70,11 +77,21 @@ public class ExcelUtil {
       return null;
    }
 
-   public static List<Object> readMoreThan1000Row(String filePath, AnalysisEventListener listener){
-      return readMoreThan1000Row(filePath,listener, null);
+   /**
+    * 读大于1000行数据
+    * @param filePath 文件觉得路径
+    * @return
+    */
+   public static List<Object> readMoreThan1000Row(String filePath){
+      return readMoreThan1000RowBySheet(filePath,null);
    }
 
-   public static List<Object> readMoreThan1000Row(String filePath, AnalysisEventListener listener,Sheet sheet){
+   /**
+    * 读大于1000行数据, 带样式
+    * @param filePath 文件觉得路径
+    * @return
+    */
+   public static List<Object> readMoreThan1000RowBySheet(String filePath, Sheet sheet){
       if(!StringUtils.hasText(filePath)){
          return null;
       }
@@ -101,52 +118,24 @@ public class ExcelUtil {
       return null;
    }
 
-   public static void writeWithTemplateAndStyle(String filePath, List<? extends BaseRowModel> data, Sheet sheet){
-      if(CollectionUtils.isEmpty(data)){
-         return;
-      }
-
-      sheet = (sheet != null) ? sheet : initSheet;
-      sheet.setClazz(data.get(0).getClass());
-
-      OutputStream outputStream = null;
-      ExcelWriter writer = null;
-      try {
-         outputStream = new FileOutputStream(filePath);
-         writer = EasyExcelFactory.getWriter(outputStream);
-         writer.write(data,sheet);
-      } catch (FileNotFoundException e) {
-         log.error("找不到文件或文件路径错误, 文件：{}", filePath);
-      }finally {
-         try {
-            if(writer != null){
-               writer.finish();
-            }
-
-            if(outputStream != null){
-               outputStream.close();
-            }
-         } catch (IOException e) {
-            log.error("excel文件读取失败, 失败原因：{}", e);
-         }
-      }
-
-   }
-
-
-   public static void writeWithTemplate(String filePath, List<? extends BaseRowModel> data){
-         writeWithTemplateAndStyle(filePath,data,null);
-
+   /**
+    * 生成excle
+    * @param filePath  绝对路径, 如：/home/chenmingjian/Downloads/aaa.xlsx
+    * @param data 数据源
+    * @param head 表头
+    */
+   public static void writeBySimple(String filePath, List<List<Object>> data, List<String> head){
+      writeSimpleBySheet(filePath,data,head,null);
    }
 
    /**
-    *
+    * 生成excle
     * @param filePath 绝对路径, 如：/home/chenmingjian/Downloads/aaa.xlsx
     * @param data 数据源
     * @param sheet excle页面样式
     * @param head 表头
     */
-   public static void writeSimpleByStle(String filePath, List<List<Object>> data, List<String> head, Sheet sheet){
+   public static void writeSimpleBySheet(String filePath, List<List<Object>> data, List<String> head, Sheet sheet){
       sheet = (sheet != null) ? sheet : initSheet;
 
       if(head != null){
@@ -180,80 +169,58 @@ public class ExcelUtil {
 
    }
 
+   /**
+    * 生成excle
+    * @param filePath 绝对路径, 如：/home/chenmingjian/Downloads/aaa.xlsx
+    * @param data 数据源
+    */
+   public static void writeWithTemplate(String filePath, List<? extends BaseRowModel> data){
+      writeWithTemplateAndSheet(filePath,data,null);
+   }
 
    /**
     * 生成excle
-    * @param filePath  绝对路径, 如：/home/chenmingjian/Downloads/aaa.xlsx
+    * @param filePath 绝对路径, 如：/home/chenmingjian/Downloads/aaa.xlsx
     * @param data 数据源
-    * @param head 表头
+    * @param sheet excle页面样式
     */
-   public static void writeBySimple(String filePath, List<List<Object>> data, List<String> head){
-      writeSimpleByStle(filePath,data,head,null);
-   }
-
-   public static void main(String[] args) {
-//      String filePath = "/home/chenmingjian/Downloads/学生表.xlsx";
-//      Sheet initSheet = new Sheet(1,0);
-//      List<Object> objects = readMoreThan1000Row(filePath);
-//      objects.forEach(System.out::println);
-
-//      Sheet sheet1 = new Sheet(1, 0);
-//      sheet1.setSheetName("学生");
-//      List<List<Object>> list = new ArrayList<>();
-//      list.add(Arrays.asList("111","222","333"));
-//      list.add(Arrays.asList("111","222","333"));
-//      list.add(Arrays.asList("111","222","333"));
-//
-//      List<String> head = Arrays.asList("111", "333", "222");
-//
-//      ExcelUtil.writeBySimple("/home/chenmingjian/Downloads/aaa.xlsx",list,head);
-
-//      ExcelUtil.write("/home/chenmingjian/Downloads/aaa.xlsx",list);
-
-
-
-
-//      ArrayList<TableHeaderExcelProperty> list = new ArrayList<>();
-//      for(int i = 0; i < 4; i++){
-//         TableHeaderExcelProperty tableHeaderExcelProperty = new TableHeaderExcelProperty();
-//         tableHeaderExcelProperty.setName("cmj" + i);
-//         tableHeaderExcelProperty.setAge(22 + i);
-//         tableHeaderExcelProperty.setSchool("清华大学" + i);
-//         list.add(tableHeaderExcelProperty);
-//      }
-//
-//      ExcelUtil.writeWithTemplate("/home/chenmingjian/Downloads/aaa.xlsx",list);
-
-      ArrayList<MultipleSheelPropety> list1 = new ArrayList<>();
-
-      for(int j = 1; j < 4; j++){
-         ArrayList<TableHeaderExcelProperty> list = new ArrayList<>();
-         for(int i = 0; i < 4; i++){
-            TableHeaderExcelProperty tableHeaderExcelProperty = new TableHeaderExcelProperty();
-            tableHeaderExcelProperty.setName("cmj" + i);
-            tableHeaderExcelProperty.setAge(22 + i);
-            tableHeaderExcelProperty.setSchool("清华大学" + i);
-            list.add(tableHeaderExcelProperty);
-         }
-
-         Sheet sheet = new Sheet(j, 0);
-         sheet.setSheetName("sheet" + j);
-
-         MultipleSheelPropety multipleSheelPropety = new MultipleSheelPropety();
-         multipleSheelPropety.setData(list);
-         multipleSheelPropety.setSheet(sheet);
-
-         list1.add(multipleSheelPropety);
-
+   public static void writeWithTemplateAndSheet(String filePath, List<? extends BaseRowModel> data, Sheet sheet){
+      if(CollectionUtils.isEmpty(data)){
+         return;
       }
 
+      sheet = (sheet != null) ? sheet : initSheet;
+      sheet.setClazz(data.get(0).getClass());
 
-      ExcelUtil.writeWithMultipleSheel("/home/chenmingjian/Downloads/aaa.xlsx",list1);
+      OutputStream outputStream = null;
+      ExcelWriter writer = null;
+      try {
+         outputStream = new FileOutputStream(filePath);
+         writer = EasyExcelFactory.getWriter(outputStream);
+         writer.write(data,sheet);
+      } catch (FileNotFoundException e) {
+         log.error("找不到文件或文件路径错误, 文件：{}", filePath);
+      }finally {
+         try {
+            if(writer != null){
+               writer.finish();
+            }
 
-
+            if(outputStream != null){
+               outputStream.close();
+            }
+         } catch (IOException e) {
+            log.error("excel文件读取失败, 失败原因：{}", e);
+         }
+      }
 
    }
 
+   /**
+    * 生成多Sheet的excle
+    * @param filePath 绝对路径, 如：/home/chenmingjian/Downloads/aaa.xlsx
+    * @param multipleSheelPropetys
+    */
    public static void writeWithMultipleSheel(String filePath,List<MultipleSheelPropety> multipleSheelPropetys){
       if(CollectionUtils.isEmpty(multipleSheelPropetys) || CollectionUtils.isEmpty(multipleSheelPropetys.get(0).getData())){
          return;
@@ -288,6 +255,9 @@ public class ExcelUtil {
 
    }
 
+
+   /*********************匿名内部类开始，可以提取出去******************************/
+
    @Data
    public static class MultipleSheelPropety{
 
@@ -296,5 +266,44 @@ public class ExcelUtil {
       private Sheet sheet;
    }
 
+   /**
+    * 解析监听器，
+    * 每解析一行会回调invoke()方法。
+    * 整个excel解析结束会执行doAfterAllAnalysed()方法
+    *
+    * @author: chenmingjian
+    * @date: 19-4-3 14:11
+    */
+   @Getter
+   @Setter
+   public static class ExcelListener extends AnalysisEventListener {
+
+      private List<Object> datas = new ArrayList<>();
+
+      /**
+       * 逐行解析
+       * object : 当前行的数据
+       */
+      @Override
+      public void invoke(Object object, AnalysisContext context) {
+         //当前行
+         // context.getCurrentRowNum()
+         if (object != null) {
+            datas.add(object);
+         }
+      }
+
+
+      /**
+       * 解析完所有数据后会调用该方法
+       */
+      @Override
+      public void doAfterAllAnalysed(AnalysisContext context) {
+         //解析结束销毁不用的资源
+      }
+
+   }
+
+   /************************匿名内部类结束，可以提取出去***************************/
 
 }
---
+++ src/main/java/com/springboot/utils/excel/Test.java
@@ -0,0 +1,153 @@
+package com.springboot.utils.excel;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.metadata.BaseRowModel;
+import com.alibaba.excel.metadata.Sheet;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @description: 测试类
+ * @author: chenmingjian
+ * @date: 19-4-4 15:24
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class Test {
+
+    /**
+     * 读取少于1000行的excle
+     */
+    @org.junit.Test
+    public void readLessThan1000Row(){
+        String filePath = "/home/chenmingjian/Downloads/测试.xlsx";
+        List<Object> objects = ExcelUtil.readLessThan1000Row(filePath);
+        objects.forEach(System.out::println);
+    }
+
+    /**
+     * 读取少于1000行的excle，可以指定sheet和从几行读起
+     */
+    @org.junit.Test
+    public void readLessThan1000RowBySheet(){
+        String filePath = "/home/chenmingjian/Downloads/测试.xlsx";
+        Sheet sheet = new Sheet(1, 1);
+        List<Object> objects = ExcelUtil.readLessThan1000RowBySheet(filePath,sheet);
+        objects.forEach(System.out::println);
+    }
+
+    /**
+     * 读取大于1000行的excle
+     * 带sheet参数的方法可参照测试方法readLessThan1000RowBySheet()
+     */
+    @org.junit.Test
+    public void readMoreThan1000Row(){
+        String filePath = "/home/chenmingjian/Downloads/测试.xlsx";
+        List<Object> objects = ExcelUtil.readMoreThan1000Row(filePath);
+        objects.forEach(System.out::println);
+    }
+
+
+    /**
+     * 生成excle
+     * 带sheet参数的方法可参照测试方法readLessThan1000RowBySheet()
+     */
+    @org.junit.Test
+    public void writeBySimple(){
+        String filePath = "/home/chenmingjian/Downloads/测试.xlsx";
+        List<List<Object>> data = new ArrayList<>();
+        data.add(Arrays.asList("111","222","333"));
+        data.add(Arrays.asList("111","222","333"));
+        data.add(Arrays.asList("111","222","333"));
+        List<String> head = Arrays.asList("表头1", "表头2", "表头3");
+        ExcelUtil.writeBySimple(filePath,data,head);
+    }
+
+
+    /**
+     * 生成excle, 带用模型
+     * 带sheet参数的方法可参照测试方法readLessThan1000RowBySheet()
+     */
+    @org.junit.Test
+    public void writeWithTemplate(){
+        String filePath = "/home/chenmingjian/Downloads/测试.xlsx";
+        ArrayList<TableHeaderExcelProperty> data = new ArrayList<>();
+        for(int i = 0; i < 4; i++){
+            TableHeaderExcelProperty tableHeaderExcelProperty = new TableHeaderExcelProperty();
+            tableHeaderExcelProperty.setName("cmj" + i);
+            tableHeaderExcelProperty.setAge(22 + i);
+            tableHeaderExcelProperty.setSchool("清华大学" + i);
+            data.add(tableHeaderExcelProperty);
+        }
+        ExcelUtil.writeWithTemplate(filePath,data);
+    }
+
+
+    /**
+     * 生成excle, 带用模型,带多个sheet
+     */
+    @org.junit.Test
+    public void writeWithMultipleSheel(){
+        ArrayList<ExcelUtil.MultipleSheelPropety> list1 = new ArrayList<>();
+        for(int j = 1; j < 4; j++){
+            ArrayList<TableHeaderExcelProperty> list = new ArrayList<>();
+            for(int i = 0; i < 4; i++){
+                TableHeaderExcelProperty tableHeaderExcelProperty = new TableHeaderExcelProperty();
+                tableHeaderExcelProperty.setName("cmj" + i);
+                tableHeaderExcelProperty.setAge(22 + i);
+                tableHeaderExcelProperty.setSchool("清华大学" + i);
+                list.add(tableHeaderExcelProperty);
+            }
+
+            Sheet sheet = new Sheet(j, 0);
+            sheet.setSheetName("sheet" + j);
+
+            ExcelUtil.MultipleSheelPropety multipleSheelPropety = new ExcelUtil.MultipleSheelPropety();
+            multipleSheelPropety.setData(list);
+            multipleSheelPropety.setSheet(sheet);
+
+            list1.add(multipleSheelPropety);
+
+        }
+
+        ExcelUtil.writeWithMultipleSheel("/home/chenmingjian/Downloads/aaa.xlsx",list1);
+
+    }
+
+
+    /*******************匿名内部类，实际开发中该对象要提取出去**********************/
+
+
+    /**
+     * @description:
+     * @author: chenmingjian
+     * @date: 19-4-3 14:44
+     */
+    @EqualsAndHashCode(callSuper = true)
+    @Data
+    public static class TableHeaderExcelProperty extends BaseRowModel {
+
+        /**
+         * value: 表头名称
+         * index: 列的号, 0表示第一列
+         */
+        @ExcelProperty(value = "姓名", index = 0)
+        private String name;
+
+        @ExcelProperty(value = "年龄",index = 1)
+        private int age;
+
+        @ExcelProperty(value = "学校",index = 2)
+        private String school;
+    }
+
+    /*******************匿名内部类，实际开发中该对象要提取出去**********************/
+
+}
